765e73f729395933c194f13be68312ead2ac0b53
davisgriffin <70169481+davisgriffin@users.noreply.github.com>
2022-08-03 01:18:21 -0400

    profile picture layout

--- webapp/src/routes/index.svelte
+++ webapp/src/routes/index.svelte
@@ -6,5 +6,12 @@
 
 <main class="h-screen">
     <img src="images/cover.jpg" alt="LD & the Independent Variable" />
-    <img src="images/pfp.jpg" alt="Liam Davis" />
+    <img class="pfp" src="images/pfp.jpg" alt="Liam Davis" />
+    <p class="intro">
+        Welcome and get excited about solo musician Liam Davis' upcoming
+        shows! Liam brings summer time fun and groove to every Jersey Shore bar
+        and restaurant featuring him. More often and not you can catch <em>LD
+        and the Independent Variable</em> the iconic father-son duo that's
+        been shredding tunes together as far back as one of them goes!
+    </p>
 </main>
