3ccf9200beeadbcb79ca4105abb6c9856ef4e100
griffindavis02 <76561199+griffindavis02@users.noreply.github.com>
2023-10-22 15:17:36 -0400

    point to paths based on current page in navbar

--- webapp/src/components/nav.svelte
+++ webapp/src/components/nav.svelte
@@ -5,10 +5,14 @@
 <nav class="flex flex-row m-4">
     <h1 class="title basis-1/2 text-3xl bold">Music with Liam Davis</h1>
     <div class="nav-links basis-1/2 flex justify-between mb-0">
-        <a class="nav-item" class:underline={current_page=='index'} href="#">Home</a>
-        <a class="nav-item" class:underline={current_page=='calendar'} href="#">Calendar</a>
-        <a class="nav-item" class:underline={current_page=='songs'} href="#">Song List</a>
-        <a class="nav-item" class:underline={current_page=='contact'} href="#">Contact</a>
+        <a class="nav-item" class:underline={current_page=='index'}
+            href={current_page=='index' ? "#" : "/"}>Home</a>
+        <a class="nav-item" class:underline={current_page=='calendar'}
+            href={current_page=='calender' ? "#" : "/calendar"}>Calendar</a>
+        <a class="nav-item" class:underline={current_page=='songs'}
+            href={current_page=='songs' ? "#" : "/songs"}>Song List</a>
+        <a class="nav-item" class:underline={current_page=='contact'}
+            href={current_page=='contact' ? "#" : "/contact"}>Contact</a>
     </div>
 </nav>
 
